8d3a9dcb7809d7a711d00d4aac425bbb392986a9
Vheeck <momohvictor62@gmail.com>
2020-04-11 10:53:23 +0100

    Challenges 1 - 3 Completed

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,73 @@
 <?php
 
+require 'period.php';
+
 function covid19ImpactEstimator($data)
 {
+  // DESTRUCTURING
+
+  $region = $data["region"];
+  $periodType = $data["periodType"];
+  $timeToElapse = $data["timeToElapse"];
+  $reportedCases = $data["reportedCases"];
+  $population = $data["population"];
+  $totalHospitalBeds = $data["totalHospitalBeds"];
+
+  // ESTIMATIONS
+  
+  // Challenge 1
+  
+  // Part 1
+  
+  $impact["currentlyInfected"] = $reportedCases * 10;
+  
+  // Part 2
+  
+  $severeImpact['currentlyInfected'] = $reportedCases * 50;
+  
+  // Part 3
+  
+  $days = toDays($periodType, $timeToElapse);
+
+  $factor = factor($days);
+
+  $impact["infectionsByRequestedTime"] = estimateInfections($factor, $impact["currentlyInfected"]);
+
+  $severeImpact["infectionsByRequestedTime"] = estimateInfections($factor, $severeImpact["currentlyInfected"]);
+  
+  // Challenge 2
+
+  // Part 1
+
+  $impact["severeCasesByRequestedTime"] = estimateSevereCases($impact["infectionsByRequestedTime"]);
+
+  $severeImpact["severeCasesByRequestedTime"] = estimateSevereCases($severeImpact["infectionsByRequestedTime"]);
+  
+  // Part 2
+
+  $impact["hospitalBedsByRequestedTime"] = estimateHospitalBeds($totalHospitalBeds, $impact["severeCasesByRequestedTime"]);
+
+  $severeImpact["hospitalBedsByRequestedTime"] = estimateHospitalBeds($totalHospitalBeds, $severeImpact["severeCasesByRequestedTime"]);
+  
+  // Challenge 3
+
+  // Part 1
+
+  $impact["casesForICUByRequestedTime"] = estimateRequiredICUs($impact["infectionsByRequestedTime"]);
+
+  $severeImpact["casesForICUByRequestedTime"] = estimateRequiredICUs($severeImpact["infectionsByRequestedTime"]);
+  
+  // Part 2
+
+  $impact["casesForVentilatorsByRequestedTime"] = estimateRequiredVentilators($impact["infectionsByRequestedTime"]);
+
+  $severeImpact["casesForVentilatorsByRequestedTime"] = estimateRequiredVentilators($severeImpact["infectionsByRequestedTime"]);
+  
+  // Part 3
+
+  $impact["dollarsInFlight"] = estimateDollarsInFlight($impact["infectionsByRequestedTime"]);
+
+  $severeImpact["dollarsInFlight"] = estimateDollarsInFlight($severeImpact["infectionsByRequestedTime"]);
+
   return $data;
 }
---
+++ src/period.php
@@ -0,0 +1,66 @@
+<?php
+    function toDays ($type, $time) {
+        switch ($type) {
+            case 'days':
+                return $time;
+                break;
+            
+            case 'weeks':
+                return $time * 7;
+                break;
+            
+            case 'months':
+                return $time * 30;
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
+    function factor ($days) {
+        $factor = floor($days / 3);
+
+        return $factor;
+    }
+
+    function estimateInfections ($factor, $currentlyInfected) {
+        $infections = $currentlyInfected * (pow(2, $factor));
+
+        return $infections;
+    }
+
+    function estimateSevereCases ($infections) {
+        $severeCases = floor($infections * (15 / 100));
+
+        return $severeCases;
+    }
+
+    function estimateHospitalBeds ($severeCases, $totalHospitalBeds) {
+        $availableBeds = floor($totalHospitalBeds * (35 / 100));
+
+        $availableBedsForCases = $availableBeds - $severeCases;
+
+        return $availableBedsForCases;
+    }
+
+    function estimateRequiredICUs ($infections) {
+        $ICUs = floor($infections * (5 / 100));
+
+        return $ICUs;
+    }
+
+    function estimateRequiredVentilators ($infections) {
+        $Ventilators = floor($infections * (2 / 100));
+
+        return $Ventilators;
+    }
+
+    function estimateDollarsInFlight ($infections, $avgIncome, $days) {
+        $dailyLose = floor($infections * (65 / 100) * $avgIncome / $days);
+
+        return $dailyLose;
+    }
+
+?>
