378d5666d656f4f319518e3fe8fcfec86dab1cc1
Vheeck <momohvictor62@gmail.com>
2020-04-13 10:31:48 +0100

    Challenges 1 - 3 Modified

--- src/period.php
+++ src/period.php
@@ -38,7 +38,7 @@
     }
 
     function estimateHospitalBeds ($totalHospitalBeds, $severeCases) {
-        $availableBeds = floor($totalHospitalBeds * (35 / 100));
+        $availableBeds = ceil($totalHospitalBeds * (35 / 100));
 
         $availableBedsForCases = $availableBeds - $severeCases;
 
